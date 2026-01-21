705f92030573a3c3f85432f29a6cc2b89b3eeb1d
DaniilPolyakow <92818258+DaniilPolyakow@users.noreply.github.com>
2026-01-21 20:43:56 +0300

    Update sender.c

--- lab9OS/sender.c
+++ lab9OS/sender.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
-#include <time.h>
-#include <string.h>
+#include <signal.h>
 
-#define SHM_SIZE 256
-#define SHM_KEY 0x1234
-#define SEM_KEY 0x5678
+int shmid, semid;
+char* shm;
 
-void sem_op(int semid, int op) {
-    struct sembuf sb = {0, op, 0};
-    semop(semid, &sb, 1);
+void cleanup(int sig) {
+    shmdt(shm);
+    shmctl(shmid, IPC_RMID, NULL);
+    semctl(semid, 0, IPC_RMID);
+    _exit(0);
 }
 
 int main() {
-    int shmid = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT | 0666);
-    char* shm = shmat(shmid, NULL, 0);
+    signal(SIGINT, cleanup);
 
-    int semid = semget(SEM_KEY, 1, IPC_CREAT | 0666);
+    shmid = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT | 0666);
+    shm = shmat(shmid, NULL, 0);
+
+    semid = semget(SEM_KEY, 1, IPC_CREAT | 0666);
     semctl(semid, 0, SETVAL, 1);
 
     while (1) {
@@ -34,8 +30,4 @@ int main() {
 
         sleep(3);
     }
-
-    shmdt(shm);
-    shmctl(shmid, IPC_RMID, NULL);
-    return 0;
 }
