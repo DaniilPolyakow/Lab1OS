a89884b782e89f02334be746bae48ffefa828e52
DaniilPolyakow <92818258+DaniilPolyakow@users.noreply.github.com>
2026-01-21 22:14:18 +0300

    Update and rename lab10_rwlock.c to main.c

--- lab10OS/main.c
+++ lab10OS/main.c
@@ -1,3 +1,4 @@
+#define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -6,62 +7,61 @@
 
 #define READERS_COUNT 10
 #define ARRAY_SIZE 64
-#define ITERATIONS 10
+#define WRITE_ITERATIONS 20
 
 char shared_array[ARRAY_SIZE];
 pthread_rwlock_t rwlock;
-int counter = 0;
 
-void* writer_thread(void* arg) {
-    for (int i = 0; i < ITERATIONS; i++) {
+void *writer_thread(void *arg)
+{
+    (void)arg;
+    static int counter = 0;
+
+    for (int i = 0; i < WRITE_ITERATIONS; i++) {
         pthread_rwlock_wrlock(&rwlock);
 
         counter++;
-        snprintf(shared_array, ARRAY_SIZE, "Запись номер: %d", counter);
+        snprintf(shared_array, ARRAY_SIZE, "Запись № %d", counter);
 
         pthread_rwlock_unlock(&rwlock);
-
         sleep(1);
     }
+
     return NULL;
 }
 
-void* reader_thread(void* arg) {
-    long tid = (long)arg;
+void *reader_thread(void *arg)
+{
+    long id = (long)arg;
 
-    for (int i = 0; i < ITERATIONS; i++) {
+    while (1) {
         pthread_rwlock_rdlock(&rwlock);
 
         printf("Читатель TID=%ld | Данные: \"%s\"\n",
-               tid, shared_array);
+               id, shared_array);
 
         pthread_rwlock_unlock(&rwlock);
-
         usleep(300000);
     }
+
     return NULL;
 }
 
-int main() {
+int main(void)
+{
     pthread_t readers[READERS_COUNT];
     pthread_t writer;
 
     pthread_rwlock_init(&rwlock, NULL);
-    strcpy(shared_array, "Пусто");
-
-    pthread_create(&writer, NULL, writer_thread, NULL);
+    snprintf(shared_array, ARRAY_SIZE, "Начальное состояние");
 
     for (long i = 0; i < READERS_COUNT; i++) {
-        pthread_create(&readers[i], NULL, reader_thread, (void*)i);
+        pthread_create(&readers[i], NULL, reader_thread, (void *)i);
     }
 
+    pthread_create(&writer, NULL, writer_thread, NULL);
     pthread_join(writer, NULL);
 
-    for (int i = 0; i < READERS_COUNT; i++) {
-        pthread_join(readers[i], NULL);
-    }
-
     pthread_rwlock_destroy(&rwlock);
-
     return 0;
 }
